d1ca65ad4a6e55f22520768f0af10f2aecd64740
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2019-07-29 18:09:34 +0200

    minor bug: columns throw exceptions in some cases

--- src/main/java/com/osm/psychology/core/Cols.java
+++ src/main/java/com/osm/psychology/core/Cols.java
@@ -1,10 +1,13 @@
 package com.osm.psychology.core;
 
+import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Cols {
     public static Set<Col> process(Col[] cols) {
-        Set<Col> cs = Set.of(cols);
+        HashSet<Col> cs = Stream.of(cols).collect(Collectors.toCollection(HashSet::new));
         if (cs.isEmpty()) cs.add(Col.ALL);
         if (cs.contains(Col.ALL)) return Set.of(Col.values());
         if (cs.contains(Col.BEFORE)) cs.addAll(Set.of(Col.GEOMETRY_BEFORE, Col.NUMBER_OF_TAGS_BEFORE, Col.TAGS_BEFORE));
@@ -13,7 +16,7 @@ public class Cols {
         if (cs.contains(Col.GEOMETRY_AFTER)) cs.addAll(Set.of(Col.GEOMETRY_TYPE_AFTER, Col.AREA_AFTER, Col.LENGTH_AFTER, Col.NUMBER_OF_POINTS_AFTER, Col.CENTROID_AFTER));
         if (cs.contains(Col.GEOMETRY)) cs.addAll(Set.of(Col.GEOMETRY_BEFORE, Col.GEOMETRY_AFTER));
         if (cs.contains(Col.TAGS)) cs.addAll(Set.of(Col.NUMBER_OF_TAGS_BEFORE, Col.TAGS_BEFORE, Col.NUMBER_OF_TAGS_AFTER, Col.TAGS_AFTER));
-        if (cs.contains(Col.BASIC_INFORMATION)) cs.addAll((Set.of(Col.OSM_ID, Col.CHANGESET_ID, Col.CONTRIBUTOR_USER_ID, Col.TIMESTAMP, Col.CONTRIBUTION_TYPE)));
+        if (cs.contains(Col.BASIC_INFORMATION)) cs.addAll(Set.of(Col.OSM_ID, Col.CHANGESET_ID, Col.CONTRIBUTOR_USER_ID, Col.TIMESTAMP, Col.CONTRIBUTION_TYPE));
         return cs;
     }
 }
