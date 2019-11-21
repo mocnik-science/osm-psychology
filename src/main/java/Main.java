543ff40dfb597da22bd85761c4afeb84aee68890
Franz-Benjamin Mocnik <mail@franz-benjamin.net>
2019-11-21 16:30:55 +0100

    main updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,8 +9,10 @@ public class Main {
         // example of a bounding box (the area to use for the output)
         BoundingBox eppelheim = new BoundingBox("Eppelheim", 8.6159, 49.3868, 8.6555, 49.4153);
 
-        // export changeset data about buildings as a csv file
+        // export entities of the buildings as a csv file
         ExportEntities.csv(new StrategyBuildings(), eppelheim, "2019-01-01", Col.LENGTH_AFTER, Col.GEOMETRY_BEFORE, Col.TAGS_AFTER, Col.TIMESTAMP);
-        ExportContributions.csv(new StrategyBuildings(), eppelheim, "2000-01-01", "2019-01-01", Col.LENGTH_AFTER, Col.GEOMETRY_BEFORE, Col.TAGS_AFTER, Col.TIMESTAMP);
+
+        // export changeset about buildings as a json file
+        ExportContributions.json(new StrategyBuildings(), eppelheim, "2000-01-01", "2019-01-01", Col.LENGTH_AFTER, Col.GEOMETRY_BEFORE, Col.TAGS_AFTER, Col.TIMESTAMP);
     }
 }
